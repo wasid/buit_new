25f4e3f42961f0d20632b9d5207f7aa5d687764e
Wasid Hossain <wasidmail@gmail.com>
2017-04-26 18:39:22 +0000

    bootswatch cdn added and refactor some css code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,75 +10,77 @@
     
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/slate/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">-->
+    <link href="/css/app.css" rel="stylesheet">
 
 
     <style>
-        body {
-            font-family: 'Lato';
-        }
+        /*body {*/
+        /*    font-family: 'Lato';*/
+        /*    padding-bottom: 60px;*/
+        /*}*/
 
-        .fa-btn {
-            margin-right: 6px;
-        }
-        body { padding-bottom: 60px; }
+        /*.fa-btn {*/
+        /*    margin-right: 6px;*/
+        /*}*/
+        /*.btn{border-radius: 30px;}*/
         
-        .navbar-default {
-        	background-color:#70a8d3;
-            color:#ffffff;
-          	border-radius:0;
-        }
+        /*.navbar-default {*/
+        /*	background-color:#70a8d3;*/
+        /*    color:#ffffff;*/
+        /*  	border-radius:0;*/
+        /*}*/
         
-        .panel .panel-heading{
-            background-color:#70a8d3;
-        }
+        /*.panel .panel-heading{*/
+        /*    background-color:#70a8d3;*/
+        /*}*/
         
-        .navbar-default .navbar-nav > li > a {
-          	color:#fff;
-        }
-        .navbar-default .navbar-nav > .active > a, .navbar-nav > .active > a:hover, .navbar-nav > .active > a:focus {
-            color: #ffffff;
-        	background-color:transparent;
-        }
+        /*.navbar-default .navbar-nav > li > a {*/
+        /*  	color:#fff;*/
+        /*}*/
+        /*.navbar-default .navbar-nav > .active > a, .navbar-nav > .active > a:hover, .navbar-nav > .active > a:focus {*/
+        /*    color: #ffffff;*/
+        /*	background-color:transparent;*/
+        /*}*/
               
-        .navbar-default .navbar-nav > li > a:hover, .nav > li > a:focus {
-            text-decoration: none;
-            background-color: #2C91DE;
-        }
+        /*.navbar-default .navbar-nav > li > a:hover, .nav > li > a:focus {*/
+        /*    text-decoration: none;*/
+        /*    background-color: #2C91DE;*/
+        /*}*/
               
-        .navbar-default .navbar-brand {
-          	color:#eeeeee;
-        }
-        .navbar-default .navbar-toggle {
-          	background-color:#eeeeee;
-        }
-        .navbar-default .icon-bar {
-          	background-color:#33aa33;
-        }
-        .bg {
-          background: url('http://coda-craven.org/wp-content/uploads/2016/10/hi_tech_planet-wide-coda-craven.jpg') no-repeat center center;
-          position: fixed;
-          width: 100%;
-          height: 350px; /*same height as jumbotron */
-          top:0;
-          left:0;
-          z-index: -1;
-        }
+        /*.navbar-default .navbar-brand {*/
+        /*  	color:#eeeeee;*/
+        /*}*/
+        /*.navbar-default .navbar-toggle {*/
+        /*  	background-color:#eeeeee;*/
+        /*}*/
+        /*.navbar-default .icon-bar {*/
+        /*  	background-color:#33aa33;*/
+        /*}*/
+        /*.bg {*/
+        /*  background: url('http://coda-craven.org/wp-content/uploads/2016/10/hi_tech_planet-wide-coda-craven.jpg') no-repeat center center;*/
+        /*  position: fixed;*/
+        /*  width: 100%;*/
+         /* height: 350px; /*same height as jumbotron 
+        /*  top:0;*/
+        /*  left:0;*/
+        /*  z-index: -1;*/
+        /*}*/
         
-        .jumbotron {
-          height: 350px;
-          color: white;
-          text-shadow: #444 0 1px 1px;
-          background:transparent;
-        }
-        .search-bar{
-            display:none;
-        }
+        /*.jumbotron {*/
+        /*  height: 350px;*/
+        /*  color: white;*/
+        /*  text-shadow: #444 0 1px 1px;*/
+        /*  background:transparent;*/
+        /*}*/
+        /*.search-bar{*/
+        /*    display:none;*/
+        /*}*/
     </style>
     <!-- datepicker -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
@@ -167,7 +169,7 @@
 		<div id="footer">
 			<div class="container">
 			<p class="navbar-text pull-left" style="color:#ffffff;">Copyright © <?php echo date("Y"); ?>&nbsp; system@bracuniversity.ac.bd &nbsp;</p>
-			<p class="navbar-text pull-right" style="color:#ffffff;">Developed by: <a style="color:#165A8E;" href="https://www.linkedin.com/in/wasid-hossain-976788100" target="_blank">M. M. Wasid Hossain</a></p>
+			<p class="navbar-text pull-right" style="color:#ffffff;">Developed by: <a href="https://www.linkedin.com/in/wasid-hossain-976788100" target="_blank">M. M. Wasid Hossain</a></p>
 			</div>
 		</div>
     </div>
